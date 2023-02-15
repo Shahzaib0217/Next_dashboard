a3ccbb230284b2c02777e7c0489cff782aeff108
shahzaib <f201079@cfd.nu.edu.pk>
2023-02-16 02:01:38 +0500

    second commit

--- my-app/components/Sidebar.jsx
+++ my-app/components/Sidebar.jsx
@@ -12,15 +12,15 @@ const Sidebar = ({ children }) => {
                 <div className='flex flex-col items-center'>
                     <Link href='/'>
                         <div className='bg-purple-800 text-white p-3 rounded-lg inline-block'>
-                            <AiOutlineHome size={20} />
-                        </div>
-                    </Link>
-                    <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
-                    <Link href='/'>
-                        <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
                             <RxDashboard size={20} />
                         </div>
                     </Link>
+                    <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
+                    {/* <Link href='/'>
+                        <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
+                            <RxDashboard size={20} />
+                        </div>
+                    </Link> */}
                     <Link href='/'>
                         <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
                             <FiSettings size={20} />
